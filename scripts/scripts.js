1dadce7dfe2e0fb519ab1abe2d260e47b21ddd68
Chahine-Chahine <chahinesquare@hotmail.com>
2023-11-17 01:15:59 +0200

    implemented testemonial section v2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -76,14 +76,32 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }    
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+// Function to automatically change slide every 5 seconds
+function autoSlide() {
+  slideIndex++;
+  showSlides(slideIndex);
+  setTimeout(autoSlide, 5000); // Change slide every 5 seconds (adjust as needed)
+}
+
+// Start the automatic slideshow
+autoSlide();
